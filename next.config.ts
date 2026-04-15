6fe4c623d05a0671620b1c380d25fb8ed30632ed
tristantamani <tristanandreitamani@gmail.com>
2026-04-15 18:22:00 +0800

    fix: resolve deployment build log error

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  distDir: 'dist',
   trailingSlash: true,
-  images: {
-    unoptimized: true,
-  },
 }
 
 export default nextConfig
